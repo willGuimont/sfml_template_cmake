ede9436ae2898909c9233ff3a5a22faca3fb6eec
William Guimont-Martin <wil9876543210@hotmail.com>
2020-07-12 18:29:55 -0400

    code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    sf::Window App(sf::VideoMode(800, 600), "sfml_test");
+
+    while (App.isOpen()) {
+        sf::Event Event;
+        while (App.pollEvent(Event)) {
+            if (Event.type == sf::Event::Closed)
+                App.close();
+        }
+        App.display();
+    }
+}
